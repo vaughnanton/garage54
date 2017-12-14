f81883fb45121a478aa351df3cb33d0bdf4493be
Vaughn Antonyan <vaughnanton@gmail.com>
2017-12-13 20:51:20 -0800

    Complete jquery animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-$(".navbar-toggler").click(function() {
-  $("nav.navbar" ).toggleClass("open");
+$(document).ready(function(){
+
+  $(".aboutDIV, .contactDIV, .showroomDIV").hide();
+
+  $("#home").click(function(){
+      $(".aboutDIV, .contactDIV, .showroomDIV").fadeOut(1000);
+  });
+
+  $("#about").click(function(){
+      $(".showroomDIV, .contactDIV").fadeOut(1500);
+      $(".aboutDIV").show(2000);
+  });
+
+  $("#showroom").click(function(){
+      $(".aboutDIV, .contactDIV").fadeOut(1500);
+      $(".showroomDIV").show(2000);
+  });
+
+  $("#contact").click(function(){
+      $(".aboutDIV, .showroomDIV").fadeOut(1500);
+      $(".contactDIV").show(2000);
+  });
 });
